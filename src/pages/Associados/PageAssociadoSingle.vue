44e9e0f53551666c50c7144ce9e83efccdf80f8c
brunobarross <altamiro_bruno@yahoo.com.br>
2024-05-06 15:02:17 -0300

    trazendo dados de conta

--- src/pages/Associados/PageAssociadoSingle.vue
+++ src/pages/Associados/PageAssociadoSingle.vue
@@ -131,6 +131,41 @@
             </div>
           </q-tab-panel>
           <q-tab-panel name="detalhes-conta" class="!overflow-hidden !p-24">
+            <div class="grid grid-cols-12 gap-24">
+       
+              <OSelect
+              v-model="models.banco"
+                label="Banco"
+                class="col-span-4"
+                size="lg"
+                :options="optBancos"
+                emit-value
+                option-value="value"
+                option-label="label"
+                map-options
+              />
+              <OSelect
+                v-model="models.convenio"
+                label="Convênio"
+                class="col-span-4"
+                size="lg"
+                :options="optConvenios"
+                emit-value
+                option-value="value"
+                option-label="label"
+                map-options
+              />
+
+              <OInputDateTime
+                :data="models.data_convenio"
+                size="lg"
+                :label="`Data do convênio `"
+                :has-time="false"
+                class="h-48 col-span-4"
+                :input-props="{
+                  // rules: [(val) => !!val || 'Campo Obrigatorio'],
+                }" />
+            </div>
           </q-tab-panel>
         </q-tab-panels>
         <div class="flex items-center justify-end p-24 gap-14">
@@ -160,7 +195,8 @@ import OSelect from 'components/Select/OSelect.vue'
 const tab = ref('dados-pessoais')
 const { URLS } = api.defaults
 const route = useRoute()
-const { getAssociado, patchDadosAssociados } = associadosService()
+const { getAssociado, patchDadosAssociados, getBancos, getConvenios } =
+  associadosService()
 const camposAlterados = ref({})
 const models = ref({
   name: '',
@@ -168,6 +204,8 @@ const models = ref({
   telefone: '',
   sexo: '',
   nascimento: '',
+  data_convenio: '',
+  convenio: '',
   cpf_cnpj: '',
   address: '',
   cep: '',
@@ -177,6 +215,7 @@ const models = ref({
   cidade: '',
   estado: '',
   pais: '',
+  banco: '',
 })
 
 let modelDefault = {}
@@ -185,6 +224,9 @@ const id = route.params.id
 
 const data = ref(null)
 
+const optBancos = ref([])
+
+const optConvenios = ref([])
 watch(
   () => data.value,
   (v) => {
@@ -200,6 +242,12 @@ watch(
     models.value.cidade = v.cidade
     models.value.estado = v.estado
     models.value.pais = v.pais
+    models.value.banco = {
+      label: v.associados_aut_set[0]?.banco_name,
+      value: v.associados_aut_set[0]?.id,
+    }
+    models.value.convenio = v.associados_aut_set[0]?.convenio
+    models.value.data_convenio = v.associados_aut_set[0]?.dt_conv_petros
 
     modelDefault = { ...models.value }
   },
@@ -249,14 +297,46 @@ const temCamposAlterados = computed(() => {
   return Object.keys(camposAlterados.value).length > 0
 })
 
-const requests = async () => {
+const getAssociadoRequest = async () => {
   try {
     data.value = await getAssociado(id)
   } catch (error) {
     console.log(error)
   }
 }
-onMounted(() => {
-  requests()
+
+const getBancosRequest = async () => {
+  try {
+    const response = await getBancos()
+
+    optBancos.value = response?.results.map((item) => ({
+      label: item.bancos,
+      value: item.id,
+    }))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getConveniosRequest = async () => {
+  try {
+    const response = await getConvenios()
+    optConvenios.value = response.map((item) => ({
+      label: item.convenio,
+      value: item.convenio,
+    }))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const requests = async () => {
+  await getAssociadoRequest()
+  await getBancosRequest()
+  await getConveniosRequest()
+}
+
+onMounted(async () => {
+  await requests()
 })
 </script>
