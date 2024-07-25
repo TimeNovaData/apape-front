cfe75a241575de5632e176c81c09b63bbc7dba33
brunobarross <altamiro_bruno@yahoo.com.br>
2024-07-25 12:08:57 -0300

    cetralizando tr

--- src/components/TR/TrAssociados.vue
+++ src/components/TR/TrAssociados.vue
@@ -3,13 +3,13 @@
     :props="props"
     class="cursor-pointer"
     @click="() => $router.push({ path: `/associados/${props.row.id}` })">
-    <q-td key="matricula">
+    <q-td key="matricula" class="text-center">
       {{ props.row.matricula }}
     </q-td>
-    <q-td key="nome">
+    <q-td key="nome" class="text-center">
       {{ props.row.name }}
     </q-td>
-    <q-td key="cpf">
+    <q-td key="cpf" class="text-center">
       {{ props.row.cpf_cnpj }}
     </q-td>
     <q-td key="patrocinadora" class="capitalize text-center">
@@ -18,28 +18,25 @@
     <q-td key="maticula_patrocinadora" class="text-center">
       {{ props.row.maticula_patrocinadora }}
     </q-td>
-    <q-td key="email">
+    <q-td key="email" class="text-center">
       {{ props.row.email }}
     </q-td>
-    <q-td key="data_cadastro">
+    <q-td key="data_cadastro" class="text-center">
       {{ FData(props.row.dt_cadastro) }}
     </q-td>
     <q-td key="tipo_beneficio" class="text-center">
-     
       <OBadge
         :label="props.row.status"
         class="w-full flex-col !rounded-[3px]"
         :class="`cor-${props.row.status?.toLowerCase()}`" />
-        <!-- :class="`text-${colorBadge}`" -->
-        <!-- :style="`background-color: rgba(var(--${colorBadge}), .10);` -->
     </q-td>
-    <q-td key="cidade">
+    <q-td key="cidade" class="text-center">
       {{ props.row.cidade }}
     </q-td>
-    <q-td key="estado">
+    <q-td key="estado" class="text-center">
       {{ props.row.estado }}
     </q-td>
-    <q-td key="pais">
+    <q-td key="pais" class="text-center">
       {{ props.row.pais }}
     </q-td>
   </q-tr>
@@ -61,8 +58,6 @@ const { props } = defineProps({
 
 const emits = defineEmits(['aprove', 'reject'])
 
-
-
 const tipoBeneficiario = computed(() => {
   const statusANegar = ['N', '', '0', 0, null, undefined]
 
@@ -74,29 +69,26 @@ const tipoBeneficiario = computed(() => {
     return 'Ativo'
   }
 })
-
 </script>
 <style scoped lang="scss">
-.cor-aposentado{
+.cor-aposentado {
   color: rgba(102, 205, 170, 1);
-  background-color: rgba(102, 205, 170, .10);
+  background-color: rgba(102, 205, 170, 0.1);
 }
-.cor-falecido{
+.cor-falecido {
   color: rgba(139, 0, 0, 1);
-  background-color: rgba(139, 0, 0, .10);
+  background-color: rgba(139, 0, 0, 0.1);
 }
-.cor-pensionista{
+.cor-pensionista {
   color: rgba(65, 105, 225, 1);
-  background-color: rgba(65, 105, 225, .10);
+  background-color: rgba(65, 105, 225, 0.1);
 }
-.cor-desfiliado{
+.cor-desfiliado {
   color: rgba(169, 169, 169, 1);
-  background-color: rgba(169, 169, 169, .10);
+  background-color: rgba(169, 169, 169, 0.1);
 }
-.cor-ativo{
+.cor-ativo {
   color: rgba(34, 139, 34, 1);
-  background-color: rgba(34, 139, 34, .10);
+  background-color: rgba(34, 139, 34, 0.1);
 }
-
-
 </style>
